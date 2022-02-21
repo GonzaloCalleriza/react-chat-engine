9abeed5e896a5a100530747be926f2cc232d8571
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-02-21 12:48:09 +0100

    Created basic structure for Chat-Engine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import {ChatEngine} from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const App = () => {
+    return (
+        <ChatEngine
+            height='100vh'
+            projectID='14ee195f-9ca5-4e56-938c-06c8350aaee0'
+            userName='Gonzlo'
+            userSecret='123456'
+            renderChatFeed={(chatAppProps) => 
+                <ChatFeed {...chatAppProps} />}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            
+        </ChatEngine>
+    );
 }
 
 export default App;
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChatFeed = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default ChatFeed;
