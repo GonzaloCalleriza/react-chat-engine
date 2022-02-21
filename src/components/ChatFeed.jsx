c3870ab4306514b4a7adbfaa75c8765ca99ea1e5
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-02-21 13:32:40 +0100

    Created structure of ChatFeed, need to finish with return

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,6 +1,40 @@
 import React from 'react';
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
 
-const ChatFeed = () => {
+const ChatFeed = ({ props }) => {
+
+    const { chats, activeChat, userName, messages } = props;
+
+    const chat = chats && chats[activeChat];
+
+    const renderMessages = () => {
+        const keys = Object.keys(messages);  // Keys are the IDs of messages
+
+        return keys.map((key, index) => {
+            const message = messages[key];
+            const lastMessageKey = index ===0 ? null : keys[index - 1];
+            const isMyMessage = userName === message.sender.userName;
+
+            return (
+                <div key={`msg_${index}`} style={{width: '100%'}}>
+                    <div className='message-block'>
+                        {
+                            isMyMessage ?
+                            <MyMessage />
+                            :
+                            <TheirMessage />
+                        }
+                    </div>
+                    <div className='read-receipts' style={{marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}>
+                        Read-receipts
+                    </div>
+                </div>
+            )
+        })
+    }
+
     return (
         <div>
             
