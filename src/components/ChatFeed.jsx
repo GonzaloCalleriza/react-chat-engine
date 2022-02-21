6f63ad9f385c706be84b5f0f1a162fd797d4bf46
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-02-21 13:41:26 +0100

    Finished ChatFeed component, moving to MyMessage

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -22,9 +22,9 @@ const ChatFeed = ({ props }) => {
                     <div className='message-block'>
                         {
                             isMyMessage ?
-                            <MyMessage />
+                            <MyMessage message={message}/>
                             :
-                            <TheirMessage />
+                            <TheirMessage message={message} lastMessage={messages[lastMessageKey]} />
                         }
                     </div>
                     <div className='read-receipts' style={{marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}>
@@ -35,9 +35,21 @@ const ChatFeed = ({ props }) => {
         })
     }
 
+    if(!chat) return 'Loading...'
+
     return (
-        <div>
-            
+        <div className='chat-feed'>
+            <div className='chat-title-container'>
+                <div className='chat-title'>{chat?.title}</div>
+                <div className='chat-subtitle'>
+                    {chat.people.map((person) => ` ${person.person.userName}`)}
+                </div>
+            </div>
+            {renderMessages()}
+            <div style={{height: '100px'}}/>
+            <div className='message-form-container'>
+                <MessageForm {...props} chatId={activeChat} />
+            </div>
         </div>
     );
 }
